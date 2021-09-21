ed90aa5486d17f3f9dd0cdb003a6ecafeb426631
Nick Gowdy <nickgowdy87@gmail.com>
2021-09-21 14:28:08 +0100

    All tests passing again.

--- test/grid_factory_test.exs
+++ test/grid_factory_test.exs
@@ -1,17 +1,19 @@
 defmodule GridFactoryTest do
   use ExUnit.Case
 
+  alias TicTacToe.{GridFactory, Entities}
+
   test "Function builds 3x3 grid for Tic Tac Toe" do
-    assert TicTacToe.GridFactory.build() == [
-             %{player: 0, x: 0, y: 0},
-             %{player: 0, x: 0, y: 1},
-             %{player: 0, x: 0, y: 2},
-             %{player: 0, x: 1, y: 0},
-             %{player: 0, x: 1, y: 1},
-             %{player: 0, x: 1, y: 2},
-             %{player: 0, x: 2, y: 0},
-             %{player: 0, x: 2, y: 1},
-             %{player: 0, x: 2, y: 2}
+    assert GridFactory.build() == [
+             %Entities.Square{player: nil, x: 0, y: 0},
+             %Entities.Square{player: nil, x: 0, y: 1},
+             %Entities.Square{player: nil, x: 0, y: 2},
+             %Entities.Square{player: nil, x: 1, y: 0},
+             %Entities.Square{player: nil, x: 1, y: 1},
+             %Entities.Square{player: nil, x: 1, y: 2},
+             %Entities.Square{player: nil, x: 2, y: 0},
+             %Entities.Square{player: nil, x: 2, y: 1},
+             %Entities.Square{player: nil, x: 2, y: 2}
            ]
   end
 end
