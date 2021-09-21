5dc3a030c2d6ebe7c16c424749df2fddd4e06d54
Nick Gowdy <nickgowdy87@gmail.com>
2021-09-21 14:23:01 +0100

    Formatting to tests.

--- test/grid_service_test.exs
+++ test/grid_service_test.exs
@@ -4,78 +4,80 @@ defmodule GridServiceTest do
   alias TicTacToe.{GridFactory, GridService}
   alias TicTacToe.Entities.Square
 
-  test "Return true when complete combination is vertical" do
-    grid = [
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 1},
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 2}
-    ]
+  describe "Logic to confirm game logic is working for all possible combinations" do
+    test "Return true when complete combination is vertical" do
+      grid = [
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 1},
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 2}
+      ]
 
-    result = GridService.is_winner(grid, 1)
-    assert result == true
-  end
+      result = GridService.is_winner(grid, 1)
+      assert result == true
+    end
 
-  test "Return false there is not a complete vertical combination" do
-    grid = [
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 1}
-    ]
+    test "Return false there is not a complete vertical combination" do
+      grid = [
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 1}
+      ]
 
-    result = GridService.is_winner(grid, 1)
-    assert result == false
-  end
+      result = GridService.is_winner(grid, 1)
+      assert result == false
+    end
 
-  test "Return true when complete combination is horizontal" do
-    grid = [
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
-      %TicTacToe.Entities.Square{player: 1, x: 1, y: 0},
-      %TicTacToe.Entities.Square{player: 1, x: 2, y: 0}
-    ]
+    test "Return true when complete combination is horizontal" do
+      grid = [
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
+        %TicTacToe.Entities.Square{player: 1, x: 1, y: 0},
+        %TicTacToe.Entities.Square{player: 1, x: 2, y: 0}
+      ]
 
-    result = GridService.is_winner(grid, 1)
-    assert result == true
-  end
+      result = GridService.is_winner(grid, 1)
+      assert result == true
+    end
 
-  test "Return false there is not a complete horizontal combination" do
-    grid = [
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
-      %TicTacToe.Entities.Square{player: 1, x: 1, y: 0}
-    ]
+    test "Return false there is not a complete horizontal combination" do
+      grid = [
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
+        %TicTacToe.Entities.Square{player: 1, x: 1, y: 0}
+      ]
 
-    result = GridService.is_winner(grid, 1)
-    assert result == false
-  end
+      result = GridService.is_winner(grid, 1)
+      assert result == false
+    end
 
-  test "Return true when complete combination in diagonal 1" do
-    grid = [
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
-      %TicTacToe.Entities.Square{player: 1, x: 1, y: 1},
-      %TicTacToe.Entities.Square{player: 1, x: 2, y: 2}
-    ]
+    test "Return true when complete combination in diagonal 1" do
+      grid = [
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
+        %TicTacToe.Entities.Square{player: 1, x: 1, y: 1},
+        %TicTacToe.Entities.Square{player: 1, x: 2, y: 2}
+      ]
 
-    result = GridService.is_winner(grid, 1)
-    assert result == true
-  end
+      result = GridService.is_winner(grid, 1)
+      assert result == true
+    end
 
-  test "Return false when x and y not equal same value for diagonal 1" do
-    grid = [
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
-      %TicTacToe.Entities.Square{player: 1, x: 1, y: 2},
-      %TicTacToe.Entities.Square{player: 1, x: 2, y: 2}
-    ]
+    test "Return false when x and y not equal same value for diagonal 1" do
+      grid = [
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 0},
+        %TicTacToe.Entities.Square{player: 1, x: 1, y: 2},
+        %TicTacToe.Entities.Square{player: 1, x: 2, y: 2}
+      ]
 
-    result = GridService.is_winner(grid, 1)
-    assert result == false
-  end
+      result = GridService.is_winner(grid, 1)
+      assert result == false
+    end
 
-  test "Return true when complete combination in diagonal 2" do
-    grid = [
-      %TicTacToe.Entities.Square{player: 1, x: 0, y: 2},
-      %TicTacToe.Entities.Square{player: 1, x: 1, y: 1},
-      %TicTacToe.Entities.Square{player: 1, x: 2, y: 0}
-    ]
+    test "Return true when complete combination in diagonal 2" do
+      grid = [
+        %TicTacToe.Entities.Square{player: 1, x: 0, y: 2},
+        %TicTacToe.Entities.Square{player: 1, x: 1, y: 1},
+        %TicTacToe.Entities.Square{player: 1, x: 2, y: 0}
+      ]
 
-    result = GridService.is_winner(grid, 1)
-    assert result == true
+      result = GridService.is_winner(grid, 1)
+      assert result == true
+    end
   end
 end
