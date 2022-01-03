816181b2ee7d39ae9b493a37400a1495c5750dd2
Nick Gowdy <nickgowdy@nicks-mbp.home>
2022-01-03 13:15:32 +0000

    Remove debug statement.

--- lib/api_web/controllers/game_controller.ex
+++ lib/api_web/controllers/game_controller.ex
@@ -5,8 +5,7 @@ defmodule TicTacToe.Web.GameController do
     json(conn, %{value: "Hello... World."})
   end
 
-  def update(conn, params) do
-    IO.inspect(params, label: "Params @@@@@@")
+  def update(conn, _params) do
     json(conn, %{value: "Value updated."})
   end
 end
