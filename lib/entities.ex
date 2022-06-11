fe60b2fe09992f874d682a44c70a790ea8d6f8d9
Nick Gowdy <nickgowdy@nicks-mbp.home>
2022-06-11 10:23:22 +0100

    Push all changes.

--- lib/entities.ex
+++ lib/entities.ex
@@ -11,10 +11,11 @@ end
 
 defmodule TicTacToe.Entities.Game do
   @derive Jason.Encoder
-  defstruct [:id, :grid]
+  defstruct [:id, :grid, :winner]
 
   @type t :: %__MODULE__{
           id: UUID,
-          grid: list(%TicTacToe.Entities.Square{})
+          grid: list(%TicTacToe.Entities.Square{}),
+          winner: non_neg_integer()
         }
 end
