3ffefb4b348e33c70111f973327d4ef55a7759d1
Nick Gowdy <nickgowdy@nicks-mbp.home>
2022-06-12 14:25:15 +0100

    Move entities to separate folder.

--- lib/entities/game.ex
+++ lib/entities/game.ex
@@ -1,14 +1,3 @@
-defmodule TicTacToe.Entities.Square do
-  @derive Jason.Encoder
-  defstruct [:x, :y, :player]
-
-  @type t :: %__MODULE__{
-          x: non_neg_integer(),
-          y: non_neg_integer(),
-          player: non_neg_integer()
-        }
-end
-
 defmodule TicTacToe.Entities.Game do
   @derive Jason.Encoder
   defstruct [:id, :grid, :winner]
